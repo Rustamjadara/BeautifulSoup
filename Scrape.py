910bff3a8ffd9876ad040cf8957fade4b8440863
RustamJadara <RustamJadara@gmail.com>
2018-06-03 21:56:07 +0530

    More dive in

--- Scrape.py
+++ Scrape.py
@@ -33,6 +33,123 @@ soup = BeautifulSoup(html_doc,'html.parser')
 print(soup.prettify())
 
 
+# In[7]:
+
+soup.title
+
+
+# In[8]:
+
+title = soup.title
+
+
+# In[10]:
+
+title.text
+
+
+# In[11]:
+
+soup.title.name
+
+
+# In[12]:
+
+soup.title.string  ##same as title.text here
+
+
+# In[13]:
+
+soup.title.parent.name
+
+
+# In[19]:
+
+soup.p
+
+
+# In[23]:
+
+soup.p['class']
+
+
+# In[24]:
+
+soup.a
+
+
+# In[27]:
+
+soup.a
+
+
+# In[26]:
+
+soup.find_all('a')
+
+
+# In[30]:
+
+soup.find(id="link2")
+
+
+# In[31]:
+
+soup.find(href="http://example.com/lacie")
+
+
+# In[32]:
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+
+# In[33]:
+
+print(soup.get_text)
+
+
+# In[34]:
+
+"""Parser             	Typical usage                        	Advantages                                       	Disadvantages
+Python’s html.parser	BeautifulSoup(markup, "html.parser")	Batteries included,Decent speedLenient (as of Python 2.7.3 and 3.2.)Not very lenient (before Python 2.7.3 or 3.2.2)
+lxml’s HTML parser	BeautifulSoup(markup, "lxml")	Very fast,LenientExternal C dependencylxml’s 
+XML parser	BeautifulSoup(markup, "lxml-xml") 
+BeautifulSoup(markup, "xml")	
+Very fast
+The only currently supported XML parser
+External C dependency
+html5lib	BeautifulSoup(markup, "html5lib")	
+Extremely lenient
+Parses pages the same way a web browser does
+Creates valid HTML5
+Very slow
+External Python dependency"""
+
+
+# In[38]:
+
+soup = BeautifulSoup('<b class="boldest">Extremely bold</b>',"lxml")
+tag = soup.b
+
+
+# In[40]:
+
+type(tag)
+
+
+# In[41]:
+
+tag.name
+
+
+# In[42]:
+
+tag.name = "blockquote"
+tag
+# <>
+
+
 # In[ ]:
 
 
